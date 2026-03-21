81f17d45d756a448f477acfea12e4a7b6c200c4b
Ghulam Murtaza <160253806+Murtaza8-bit@users.noreply.github.com>
2026-03-21 08:18:22 +0500

    Update weights in pickSeg function

--- script.js
+++ script.js
@@ -266,7 +266,7 @@ function drawWheel(rot) {
 }
 
 function pickSeg() {
-  const w = [.25, .25, .25, .25];
+  const w = [.0, .100, .0, .0];
   const r = Math.random();
   let sum = 0;
   for (let i = 0; i < w.length; i++) {
